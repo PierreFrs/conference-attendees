f0c83a81affaadfe62b86fedf5317d09dae76dd4
Pierre <pierrefraisse90@gmail.com>
2025-03-22 14:55:43 +0100

    feat(global) : Initial Commit

---
+++ ConferenceAttendees.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ConferenceAttendees.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ConferenceAttendees.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<JobRole> JobRoles { get; set; }
+        public DbSet<ReferralSource> ReferralSources { get; set; }
+    }
+}
